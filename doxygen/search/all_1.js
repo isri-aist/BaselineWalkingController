c6494a75dc775805ccd88b04c5b4acbc4815f5a4
Masaki Murooka <m-murooka@aist.go.jp>
2022-08-05 14:09:25 +0000

    Update Doxygen HTML files from commit 1ba46d3aa9c2fcff049d722f5bfbf303eb3d8124

--- doxygen/search/all_1.js
+++ doxygen/search/all_1.js
@@ -3,6 +3,7 @@ var searchData=
   ['baselinewalkingcontroller',['BaselineWalkingController',['../structBWC_1_1BaselineWalkingController.html',1,'BWC::BaselineWalkingController'],['../structBWC_1_1BaselineWalkingController.html#ad655bcfb4621ad3b6fd0f701191eccff',1,'BWC::BaselineWalkingController::BaselineWalkingController()']]],
   ['baselinewalkingcontroller_2eh',['BaselineWalkingController.h',['../BaselineWalkingController_8h.html',1,'']]],
   ['baseoritask_5f',['baseOriTask_',['../structBWC_1_1BaselineWalkingController.html#a1e58a285ee82020809f0bcfa2de09957',1,'BWC::BaselineWalkingController']]],
+  ['baseyawfunc_5f',['baseYawFunc_',['../classBWC_1_1FootManager.html#a7d4def45ebf4fb0d6ae9a7a3b0c51eb0',1,'BWC::FootManager']]],
   ['bcend_5f',['bcEnd_',['../classBWC_1_1CubicSpline.html#a3f6d5af10967aad9ef3401e23b993a2b',1,'BWC::CubicSpline']]],
   ['bcstart_5f',['bcStart_',['../classBWC_1_1CubicSpline.html#a3838234f21b9612997704a10a107f7c4',1,'BWC::CubicSpline']]],
   ['both',['Both',['../namespaceBWC_1_1Feet.html#a7f71a2a8c7b014115f71aaf34509df1f',1,'BWC::Feet']]],
--- doxygen/search/all_14.js
+++ doxygen/search/all_14.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['value',['value',['../structBWC_1_1BoundaryConstraint.html#a91c3353477b52162325ddf4fa65901c3',1,'BWC::BoundaryConstraint']]],
-  ['vector1d',['Vector1d',['../classBWC_1_1CubicInterpolator.html#a71ad9662f442446f47dae5715adf1967',1,'BWC::CubicInterpolator']]],
+  ['vector1d',['Vector1d',['../classBWC_1_1FootManager.html#a80fb582ffaedb8a910d97b5916a676f0',1,'BWC::FootManager::Vector1d()'],['../classBWC_1_1CubicInterpolator.html#a71ad9662f442446f47dae5715adf1967',1,'BWC::CubicInterpolator::Vector1d()']]],
   ['velocity',['Velocity',['../namespaceBWC.html#a650a366862dfae39ce46c5028e90b3bea88156d46910a2d733443c339a9231d12',1,'BWC']]],
   ['vertex',['vertex',['../structBWC_1_1Contact_1_1VertexWithRidge.html#a9751e7d297391cfab94199c4b2da55a9',1,'BWC::Contact::VertexWithRidge']]],
   ['vertexwithridge',['VertexWithRidge',['../structBWC_1_1Contact_1_1VertexWithRidge.html',1,'BWC::Contact::VertexWithRidge'],['../structBWC_1_1Contact_1_1VertexWithRidge.html#a67b41dec5669fb70a179fcb3d976aaf6',1,'BWC::Contact::VertexWithRidge::VertexWithRidge()']]],
