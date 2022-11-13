526c1bb70e4175960ebb647ec6ac2e6b5a1d6f88
Masaki Murooka <m-murooka@aist.go.jp>
2022-11-13 06:35:33 +0000

    Update Doxygen HTML files from commit 56cf7b9c58165def0798f36245b4d601f7181d0c

--- doxygen/search/all_1.js
+++ doxygen/search/all_1.js
@@ -4,7 +4,7 @@ var searchData=
   ['baselinewalkingcontroller_2eh_10',['BaselineWalkingController.h',['../BaselineWalkingController_8h.html',1,'']]],
   ['baseoritask_5f_11',['baseOriTask_',['../structBWC_1_1BaselineWalkingController.html#a1e58a285ee82020809f0bcfa2de09957',1,'BWC::BaselineWalkingController']]],
   ['baseoritaskstiffness_5f_12',['baseOriTaskStiffness_',['../structBWC_1_1InitialState.html#af77ef0e68b0fb94f51a0b45ec12fba43',1,'BWC::InitialState']]],
-  ['baseyawfunc_5f_13',['baseYawFunc_',['../classBWC_1_1FootManager.html#ad0d36f5c653c87d22f1fa06ccc5d3745',1,'BWC::FootManager']]],
+  ['baseyawfunc_5f_13',['baseYawFunc_',['../classBWC_1_1FootManager.html#a874dee9b58837127d95334fa57164ddf',1,'BWC::FootManager']]],
   ['bcend_5f_14',['bcEnd_',['../classBWC_1_1CubicSpline.html#a3f6d5af10967aad9ef3401e23b993a2b',1,'BWC::CubicSpline']]],
   ['bcstart_5f_15',['bcStart_',['../classBWC_1_1CubicSpline.html#a3838234f21b9612997704a10a107f7c4',1,'BWC::CubicSpline']]],
   ['bfp_16',['BFP',['../namespaceBFP.html',1,'']]],
--- doxygen/search/variables_1.js
+++ doxygen/search/variables_1.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['baseoritask_5f_443',['baseOriTask_',['../structBWC_1_1BaselineWalkingController.html#a1e58a285ee82020809f0bcfa2de09957',1,'BWC::BaselineWalkingController']]],
   ['baseoritaskstiffness_5f_444',['baseOriTaskStiffness_',['../structBWC_1_1InitialState.html#af77ef0e68b0fb94f51a0b45ec12fba43',1,'BWC::InitialState']]],
-  ['baseyawfunc_5f_445',['baseYawFunc_',['../classBWC_1_1FootManager.html#ad0d36f5c653c87d22f1fa06ccc5d3745',1,'BWC::FootManager']]],
+  ['baseyawfunc_5f_445',['baseYawFunc_',['../classBWC_1_1FootManager.html#a874dee9b58837127d95334fa57164ddf',1,'BWC::FootManager']]],
   ['bcend_5f_446',['bcEnd_',['../classBWC_1_1CubicSpline.html#a3f6d5af10967aad9ef3401e23b993a2b',1,'BWC::CubicSpline']]],
   ['bcstart_5f_447',['bcStart_',['../classBWC_1_1CubicSpline.html#a3838234f21b9612997704a10a107f7c4',1,'BWC::CubicSpline']]],
   ['both_448',['Both',['../namespaceBWC_1_1Feet.html#a7f71a2a8c7b014115f71aaf34509df1f',1,'BWC::Feet']]]
