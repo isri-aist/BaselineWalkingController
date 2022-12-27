33154a0016d2f851a6da0ac6c91bf0be7325f8d9
Masaki Murooka <m-murooka@aist.go.jp>
2022-12-27 02:45:48 +0000

    Update Doxygen HTML files from commit 2c51db0c1f5463815dd34347ed5dc7dccc9ba63c

--- doxygen/search/all_13.js
+++ doxygen/search/all_13.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['update_255',['update',['../classBWC_1_1CentroidalManager.html#ae7381f23abe55de2a5f6814b0228b6f8',1,'BWC::CentroidalManager::update()'],['../classBWC_1_1FootManager.html#ae993f4095846c482056c688e9443e340',1,'BWC::FootManager::update()'],['../structBWC_1_1FirstOrderImpedanceTask.html#ada301f576b5e5a2364914c16d71292e2',1,'BWC::FirstOrderImpedanceTask::update()']]],
   ['updatefoottraj_256',['updateFootTraj',['../classBWC_1_1FootManager.html#a6b6516e377feb329b5683d8fc70e3650',1,'BWC::FootManager']]],
-  ['updatewalkatrelativevel_257',['updateWalkAtRelativeVel',['../classBWC_1_1FootManager.html#af5e3a8760064f05f1a0735d50e9f2345',1,'BWC::FootManager']]],
+  ['updatevelmode_257',['updateVelMode',['../classBWC_1_1FootManager.html#a28b0295a82acf276af0856890901b200',1,'BWC::FootManager']]],
   ['updatezmptraj_258',['updateZmpTraj',['../classBWC_1_1FootManager.html#af4a25307dba3da51d6a16af65f083d2a',1,'BWC::FootManager']]],
   ['useactualcomforwrenchdist_259',['useActualComForWrenchDist',['../structBWC_1_1CentroidalManager_1_1Configuration.html#ab69950e6163f7e54eb570ca9d931d5eb',1,'BWC::CentroidalManager::Configuration']]],
   ['useactualstateformpc_260',['useActualStateForMpc',['../structBWC_1_1CentroidalManager_1_1Configuration.html#a2ca257faee34e51bbd5145eb0d82875a',1,'BWC::CentroidalManager::Configuration']]],
