964928b1f9b9e80214afbecc30706ff593c4f27d
Masaki Murooka <m-murooka@aist.go.jp>
2023-01-16 06:10:01 +0000

    Update Doxygen HTML files from commit 5e7f1c458203cba06bec649d2b0aaa1be4488956

--- doxygen/search/variables_10.js
+++ doxygen/search/variables_10.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['qpcoeff_5f_615',['qpCoeff_',['../classBWC_1_1WrenchDistribution.html#a70f99228063dfd02242789c9d877811c',1,'BWC::WrenchDistribution']]],
-  ['qpsolver_5f_616',['qpSolver_',['../classBWC_1_1WrenchDistribution.html#aa88b4de6219a0a1a582eb3a0851be649',1,'BWC::WrenchDistribution']]],
-  ['qpsolvertype_617',['qpSolverType',['../structBWC_1_1CentroidalManagerIntrinsicallyStableMpc_1_1Configuration.html#a670775bd151046f08eef84ee1f57e689',1,'BWC::CentroidalManagerIntrinsicallyStableMpc::Configuration']]]
+  ['qpcoeff_5f_613',['qpCoeff_',['../classBWC_1_1WrenchDistribution.html#a70f99228063dfd02242789c9d877811c',1,'BWC::WrenchDistribution']]],
+  ['qpsolver_5f_614',['qpSolver_',['../classBWC_1_1WrenchDistribution.html#aa88b4de6219a0a1a582eb3a0851be649',1,'BWC::WrenchDistribution']]],
+  ['qpsolvertype_615',['qpSolverType',['../structBWC_1_1CentroidalManagerIntrinsicallyStableMpc_1_1Configuration.html#a670775bd151046f08eef84ee1f57e689',1,'BWC::CentroidalManagerIntrinsicallyStableMpc::Configuration']]]
 ];
--- doxygen/search/variables_11.js
+++ doxygen/search/variables_11.js
@@ -1,14 +1,14 @@
 var searchData=
 [
-  ['refcomz_618',['refComZ',['../structBWC_1_1CentroidalManager_1_1Configuration.html#a7cd2c2d2bca99f7b679da4900501cbcd',1,'BWC::CentroidalManager::Configuration']]],
-  ['refzmp_5f_619',['refZmp_',['../classBWC_1_1CentroidalManager.html#a28c2e717ae3c5b4200630fbbae32b6c9',1,'BWC::CentroidalManager']]],
-  ['regularweight_620',['regularWeight',['../structBWC_1_1WrenchDistribution_1_1Configuration.html#addb8c33e01f86d9738a9cb1aeb0fac52',1,'BWC::WrenchDistribution::Configuration']]],
-  ['requireimpgainupdate_5f_621',['requireImpGainUpdate_',['../classBWC_1_1FootManager.html#a8342fb81cf38899ff7bd58b7232e579e',1,'BWC::FootManager']]],
-  ['resulttotalwrench_5f_622',['resultTotalWrench_',['../classBWC_1_1WrenchDistribution.html#af37bcb672acd23cb22624bbd466ce554',1,'BWC::WrenchDistribution']]],
-  ['resultwrenchratio_5f_623',['resultWrenchRatio_',['../classBWC_1_1WrenchDistribution.html#accdf9022ce35ba938c33b3587ffe5c2e',1,'BWC::WrenchDistribution']]],
-  ['ridgeforceminmax_624',['ridgeForceMinMax',['../structBWC_1_1WrenchDistribution_1_1Configuration.html#a6d6ed77491b81806410805860a85ae45',1,'BWC::WrenchDistribution::Configuration']]],
-  ['ridgelist_625',['ridgeList',['../structBWC_1_1Contact_1_1VertexWithRidge.html#abfaaa9dc8d1c4b0c92cf7715829c2c69',1,'BWC::Contact::VertexWithRidge']]],
-  ['robotmass_5f_626',['robotMass_',['../classBWC_1_1CentroidalManager.html#a73798dded16013c2186d63a5b4591033',1,'BWC::CentroidalManager']]],
-  ['rotfunc_5f_627',['rotFunc_',['../classBWC_1_1SwingTrajCubicSplineSimple.html#a80d9cd4a0c5d69451ab2fe8e5fd85fa7',1,'BWC::SwingTrajCubicSplineSimple::rotFunc_()'],['../classBWC_1_1SwingTrajIndHorizontalVertical.html#a8491a9290badae1003c26bab880f2a9b',1,'BWC::SwingTrajIndHorizontalVertical::rotFunc_()']]],
-  ['running_5f_628',['running_',['../structBWC_1_1FootstepPlannerState.html#ae5fdf5d556dc87e0428286f06e24b721',1,'BWC::FootstepPlannerState']]]
+  ['refcomz_616',['refComZ',['../structBWC_1_1CentroidalManager_1_1Configuration.html#a7cd2c2d2bca99f7b679da4900501cbcd',1,'BWC::CentroidalManager::Configuration']]],
+  ['refzmp_5f_617',['refZmp_',['../classBWC_1_1CentroidalManager.html#a28c2e717ae3c5b4200630fbbae32b6c9',1,'BWC::CentroidalManager']]],
+  ['regularweight_618',['regularWeight',['../structBWC_1_1WrenchDistribution_1_1Configuration.html#addb8c33e01f86d9738a9cb1aeb0fac52',1,'BWC::WrenchDistribution::Configuration']]],
+  ['requireimpgainupdate_5f_619',['requireImpGainUpdate_',['../classBWC_1_1FootManager.html#a8342fb81cf38899ff7bd58b7232e579e',1,'BWC::FootManager']]],
+  ['resulttotalwrench_5f_620',['resultTotalWrench_',['../classBWC_1_1WrenchDistribution.html#af37bcb672acd23cb22624bbd466ce554',1,'BWC::WrenchDistribution']]],
+  ['resultwrenchratio_5f_621',['resultWrenchRatio_',['../classBWC_1_1WrenchDistribution.html#accdf9022ce35ba938c33b3587ffe5c2e',1,'BWC::WrenchDistribution']]],
+  ['ridgeforceminmax_622',['ridgeForceMinMax',['../structBWC_1_1WrenchDistribution_1_1Configuration.html#a6d6ed77491b81806410805860a85ae45',1,'BWC::WrenchDistribution::Configuration']]],
+  ['ridgelist_623',['ridgeList',['../structBWC_1_1Contact_1_1VertexWithRidge.html#abfaaa9dc8d1c4b0c92cf7715829c2c69',1,'BWC::Contact::VertexWithRidge']]],
+  ['robotmass_5f_624',['robotMass_',['../classBWC_1_1CentroidalManager.html#a73798dded16013c2186d63a5b4591033',1,'BWC::CentroidalManager']]],
+  ['rotfunc_5f_625',['rotFunc_',['../classBWC_1_1SwingTrajCubicSplineSimple.html#a80d9cd4a0c5d69451ab2fe8e5fd85fa7',1,'BWC::SwingTrajCubicSplineSimple::rotFunc_()'],['../classBWC_1_1SwingTrajIndHorizontalVertical.html#a8491a9290badae1003c26bab880f2a9b',1,'BWC::SwingTrajIndHorizontalVertical::rotFunc_()']]],
+  ['running_5f_626',['running_',['../structBWC_1_1FootstepPlannerState.html#ae5fdf5d556dc87e0428286f06e24b721',1,'BWC::FootstepPlannerState']]]
 ];
