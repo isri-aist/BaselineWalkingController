3f5ceb4305f57042d12118c5e717c510101180c2
Masaki Murooka <m-murooka@aist.go.jp>
2023-01-22 16:33:07 +0900

    Delete the original trajectory codes.

--- src/trajectory/CubicSpline.cpp
+++
@@ -1,7 +0,0 @@
-#include <BaselineWalkingController/trajectory/CubicSpline.h>
-
-namespace BWC
-{
-template class CubicSpline<Eigen::Vector3d>;
-template class CubicSpline<Eigen::VectorXd>;
-} // namespace BWC
